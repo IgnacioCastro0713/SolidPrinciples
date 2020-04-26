9f8787132dc2698a3d7b513ca4f912c240c8dc3a
ignacio <ignaciomenchaca666@gmail.com>
2020-04-25 23:16:28 -0500

    LSP-LSP-Violation add test

---
+++ TestRectangle/Tests.cs
@@ -0,0 +1,21 @@
+﻿using _03.LSP.Shapes;
+using NUnit.Framework;
+
+namespace TestRectangle
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Rectangle rectangle = new Square();
+            rectangle.Width = 3;
+            rectangle.Height = 6;
+
+            const double expected = 18;
+            var actual = rectangle.Area();
+
+            Assert.AreEqual(expected, actual);        }
+    }
+}

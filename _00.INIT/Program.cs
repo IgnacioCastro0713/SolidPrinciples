554bacafbf337c24bb2b55eb023873088a0941f1
ignacio <ignaciomenchaca666@gmail.com>
2020-04-25 19:55:09 -0500

    initial project completed

--- _00.INIT/Program.cs
+++ _00.INIT/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using _00.INIT.Shapes;
 
 namespace _00.INIT
 {
@@ -6,7 +7,21 @@ namespace _00.INIT
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine();
+            var rectangles = new[]
+            {
+                new Rectangle {Width = 10, Height = 5},
+                new Rectangle {Width = 4, Height = 6},
+                new Rectangle {Width = 5, Height = 1},
+                new Rectangle {Width = 8, Height = 9}
+            };
+
+            var sumAreas = Rectangle.SumAreas(rectangles);
+            var sumPerimeters = Rectangle.SumPerimeters(rectangles);
+
+            Console.WriteLine($"Total Area: {sumAreas}");
+            Console.WriteLine($"Total Perimeters: {sumPerimeters}");
+
+            Console.ReadKey();
         }
     }
 }
